6b99900f551631d04ea7558d1a3ce3044206b49b
DESKTOP-CI1Q5ER\andre <andrew@brbwall.com>
2018-06-04 22:39:05 -0400

    server changes

--- app/src/main/java/com/example/plcus/multimedia/ServerHTTPD.java
+++ app/src/main/java/com/example/plcus/multimedia/ServerHTTPD.java
@@ -1,12 +1,6 @@
 package com.example.plcus.multimedia;
 
-import android.os.Environment;
 import android.util.Log;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 
 import fi.iki.elonen.NanoHTTPD;
 
@@ -20,26 +14,16 @@ public class ServerHTTPD extends NanoHTTPD {
 
     @Override
     public Response serve(IHTTPSession session) {
-        String answer = "";
+        String msg = "Hello, you have connected.";
         try {
-            // Open file from SD Card
-            File root = Environment.getExternalStorageDirectory();
-            // A changer, stream la musique au lieu
-            FileReader index = new FileReader(root.getAbsolutePath() +
-                    "/www/index.html");
-            BufferedReader reader = new BufferedReader(index);
-            String line = "";
-            while ((line = reader.readLine()) != null) {
-                answer += line;
-            }
-            reader.close();
+            // Fetch JSON song files
 
-        } catch(IOException ioe) {
-            Log.w("Httpd", ioe.toString());
+        } catch(Exception e) {
+            Log.w("Httpd", e.toString());
         }
 
 
-        return newFixedLengthResponse(answer);
+        return newFixedLengthResponse(msg);
     }
 }
 
